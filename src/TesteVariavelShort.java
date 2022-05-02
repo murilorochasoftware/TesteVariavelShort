3bb935423b2e011159ae2c0dd6cc7ec6b6a41720
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:36:48 -0300

    Teste do uso de variável com o tipo de dados short em Java.

---
+++ src/TesteVariavelShort.java
@@ -0,0 +1,9 @@
+public class TesteVariavelShort {
+
+    public static void main(String[] args) {
+        short totalAlunos = 1259;
+
+        System.out.printf("O total de alunos é: %d", totalAlunos);
+
+    }
+}
